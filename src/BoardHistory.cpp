0ad8f77b2a35e73a3b5344d0402d3191121a93cf
yuichapi <antoinekahn@proton.me>
2025-12-27 11:43:43 +0100

    ajout limite taille history dans bordhistory.cpp

--- src/BoardHistory.cpp
+++ src/BoardHistory.cpp
@@ -18,7 +18,19 @@ BoardHistory::~BoardHistory()
 
 void BoardHistory::push(const BoardSnapshot* boardSnapshot)
 {
-    this->boardSnapshotsForUndo.push_back(boardSnapshot);
+
+    if (boardSnapshotsForUndo.size() > 100)     
+    {
+        delete this->boardSnapshotsForUndo.front();
+        this->boardSnapshotsForUndo.erase(this->boardSnapshotsForUndo.begin());
+        this->boardSnapshotsForUndo.push_back(boardSnapshot);
+    }
+    else
+    {
+        this->boardSnapshotsForUndo.push_back(boardSnapshot);
+    }
+
+
     while (not this->boardSnapshotsForRedo.empty())
     {
         delete this->boardSnapshotsForRedo.back();
